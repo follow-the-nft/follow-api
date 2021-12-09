bb8015c03bd0bef065d5a932ce40dc7338fa2a73
MuckT <mcgtom10@gmail.com>
2021-12-09 01:16:28 -0800

    feat: update public routes

---
+++ src/routes/public-routes.test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+require('dotenv').config();
+const supertest = require('supertest');
+const { server } = require('../server');
+const mockRequest = supertest(server);
+
+let nftId = '20512672236384795134598454803080694359308106914252699625353424791001018400769';
+
+describe('Given GET', () => {
+  describe("When'/nft/:id", () => {
+    describe('valid request', () => {
+      let response;
+      beforeAll(async () => {
+        response = await mockRequest.get(`/nft/${nftId}`);
+      });
+      it('Then should return 200 response', () => {
+        expect(response.status).toStrictEqual(200);
+      });
+
+      it('Then should return one NFT', () => {
+        expect(response.body.length).toEqual(1);
+      });
+    });
+
+    describe('invalid request', () => {
+      let response;
+      beforeAll(async () => {
+        response = await mockRequest.get('/nft/iDoNotExist');
+      });
+      it('Then should return 500 response', () => {
+        expect(response.status).toStrictEqual(500);
+      });
+    });
+  });
+});
--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
-'use strict'
+'use strict';
 
-const express = require('express')
+// Import express
+const express = require('express');
 
-const app = express()
+// Import middleware and routes
+const errorHandler = require('./handlers/error-handler');
+const publicRoutes = require('./routes/public-routes');
 
-app.use(express.json())
+// Prepare the express app
+const app = express();
 
+// App level middleware
+app.use(express.json());
+app.use(errorHandler);
+
+// Proof of life route
 app.get('/', (req, res) => {
-  res.status(200).send('@FollowTheNFT')
-})
+  res.status(200).send('@FollowTheNFT');
+});
+
+// Routes
+app.use(publicRoutes);
+
+// Handlers TODO
 
 module.exports = {
   server: app,
   start: port => app.listen(port, console.log(`Server started on Port ${port}`))
-}
+};
