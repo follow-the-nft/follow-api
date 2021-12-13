a38e429dbc9101843133844069ce6432bd4e4cf5
MuckT <mcgtom10@gmail.com>
2021-12-12 22:49:46 -0800

    feat: refactor public routes to use node-fetch

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const axios = require('axios');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 const express = require('express');
@@ -14,8 +14,9 @@ publicRouter.get('/nft/:id', async (req, res, next) => {
     if(!/^[0-9]{77}$/g.test(req.params.id)) {
       throw new Error("Please enter an id .e.g '/nft/20512672236384795134598454803080694359308106914252699625353424791001018400769'");
     }
-    let response = await axios.get(`${OPENSEA_API_URL}/assets?order_direction=desc&offset=0&token_ids=${req.params.id}`);
-    res.status(200).json(response.data.assets);
+    let response = await fetch(`${OPENSEA_API_URL}/assets?order_direction=desc&offset=0&token_ids=${req.params.id}`);
+    const json = await response.json();
+    res.status(200).json(json);
   } catch(err) {
     next(err);
   }
@@ -27,8 +28,9 @@ publicRouter.get('/address/:address', async (req, res, next) => {
     if(!/^0x[a-fA-F0-9]{40}$/g.test(req.params.address)) {
       throw new Error("Please enter an address e.g. '/address/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270'");
     }
-    let response = await axios.get(`${OPENSEA_API_URL}/assets?owner=${req.params.address}&order_direction=desc&offset=0`);
-    res.status(200).json(response.data);
+    let response = await fetch(`${OPENSEA_API_URL}/assets?owner=${req.params.address}&order_direction=desc&offset=0`);
+    const json = await response.json();
+    res.status(200).json(json);
   } catch (err) {
     next(err);
   }
@@ -38,7 +40,7 @@ publicRouter.get('/address/:address', async (req, res, next) => {
 publicRouter.get('/addresses/:addresses', async (req, res, next) => {
   try {
     if(!/[0xa-fA-F0-9]{42},?/g.test(req.params.addresses)) {
-      throw new Error("Please enter an addresses separated by commas e.g. '/address/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270,0x495f947276749ce646f68ac8c248420045cb7b5e'");
+      throw new Error("Please enter an addresses separated by commas e.g. '/addresses/0xa7d8d9ef8d8ce8992df33d8b8cf4aebabd5bd270,0x495f947276749ce646f68ac8c248420045cb7b5e'");
     }
     // TODO validate address before splitting
     let addressList = req.params.addresses.split(',').map(address => address.toLowerCase());
@@ -52,8 +54,9 @@ publicRouter.get('/addresses/:addresses', async (req, res, next) => {
       }
       addressesString += `&asset_contract_addresses=${address}`;
     });
-    let response = await axios.get(`${OPENSEA_API_URL}/assets?order_direction=desc&offset=0&${addressesString}`);
-    res.status(200).json(response.data);
+    let response = await fetch(`${OPENSEA_API_URL}/assets?order_direction=desc&offset=0&${addressesString}`);
+    const json = await response.json();
+    res.status(200).json(json);
   } catch (err) {
     next(err);
   }
@@ -63,8 +66,9 @@ publicRouter.get('/addresses/:addresses', async (req, res, next) => {
 publicRouter.get('/collection/:slug', async (req, res, next) => {
   try {
     // TODO add some validation and return 404 when nothing is found
-    let response = await axios.get(`${OPENSEA_API_URL}/collection/${req.params.slug}`);
-    res.status(200).json(response.data);
+    let response = await fetch(`${OPENSEA_API_URL}/collection/${req.params.slug}`);
+    const json = await response.json();
+    res.status(200).json(json);
   } catch (err) {
     next(err);
   }
