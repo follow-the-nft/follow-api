9a30eeedb48108cd4a771e1fdd5967dec7e3ef33
MuckT <mcgtom10@gmail.com>
2021-12-12 23:09:08 -0800

    chore: fix lint issues

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 // Import express
 const express = require('express');
 // Import cors
-const cors = require('cors')
+const cors = require('cors');
 
 // Import middleware and routes
 const errorHandler = require('./handlers/error');
@@ -15,7 +15,7 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 // App middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 // Routes
