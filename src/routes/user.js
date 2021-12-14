3ec5016bc00e4183cddb02583acfb4ea3ce971e7
MuckT <mcgtom10@gmail.com>
2021-12-13 20:44:08 -0800

    feat: user likes and follows updates

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const userRouter = express.Router();
 
-const { users } = require('../models/index');
+const { users, db } = require('../models/index');
 const bearer = require('../middleware/bearer');
 const basic = require('../middleware/basic');
 
-// GET User - User Reads 
+// GET User - User Reads
 userRouter.get('/user', async(req, res) => {
   const userData = await users.findOne({ where: {username: req.body.username}});
   res.status(200).json(userData);
@@ -36,16 +36,25 @@ userRouter.post('/register', async (req, res, next) => {
   }
 });
 
-userRouter.patch('/nft/likethis/:id', async (req, res, next) => {
+
+// Users NFT Routes ('likes')
+// TODO: GET all likes
+// POST: Add to likes
+userRouter.post('/nft/like/:id', bearer, async (req, res, next) => {
   try {
-    if(!req.params.id) return;
+    if(!req.params.id) { throw new Error('Please provide a valid ID');}
 
-    req.body['likes'] = [req.params.id];
+    // Stretch: If you already like something just unlike it.
+    let userQuery = await users.findOne({ where: {username: req.body.username}})
+    if(userQuery.likes.includes(req.params.id)) {
+      throw new Error('nft already liked!')
+    }
 
-    let record = await users.findOne({ where: { username: req.body.username}})
-
-    let userRecord = await record.update(req.body);
-    console.log('userRecord:', userRecord);
+    // Append to existing array https://stackoverflow.com/a/44365347
+    let userRecord = await users.update(
+      {'likes': db.fn('array_append', db.col('likes'), req.params.id)},
+      {'where': {'username': req.body.username}}
+    );
     
     const output = {
       user: userRecord,
@@ -57,5 +66,37 @@ userRouter.patch('/nft/likethis/:id', async (req, res, next) => {
     next(err.message);
   }
 });
+// TODO: DELETE nft like
+
+// Users Followed Address Routes ('follows')
+// TODO: Get all follows
+// POST: Add to follows
+userRouter.post('/address/follows/:address', bearer, async (req, res, next) => {
+  try {
+    if(!req.params.id) { throw new Error('Please provide a valid address');}
+
+    // Stretch: If you already like something just unlike it.
+    let userQuery = await users.findOne({ where: {username: req.body.username}})
+    if(userQuery.follows.includes(req.params.address)) {
+      throw new Error('nft already liked!')
+    }
+
+    // https://stackoverflow.com/a/44365347
+    let userRecord = await users.update(
+      {'follows': db.fn('array_append', db.col('follows'), req.params.address)},
+      {'where': {'username': req.body.username}}
+    );
+    
+    const output = {
+      user: userRecord,
+      userToken: userRecord.token,
+    };
+
+    res.status(201).json(output);
+  } catch (err) {
+    next(err.message);
+  }
+});
+// TODO DELETE follows
 
 module.exports = userRouter;
