cb9082786703c5ee446a6196d0441fd443e31ee7
MuckT <mcgtom10@gmail.com>
2021-12-13 21:27:27 -0800

    feat: toggle likes and follows

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,23 +5,6 @@ const userRouter = express.Router();
 
 const { users, db } = require('../models/index');
 const bearer = require('../middleware/bearer');
-const basic = require('../middleware/basic');
-
-// GET User - User Reads
-userRouter.get('/user', async(req, res) => {
-  const userData = await users.findOne({ where: {username: req.body.username}});
-  res.status(200).json(userData);
-});
- 
-userRouter.get('/userBearer', bearer, async(req, res) => {
-  const user = await users.findOne({ where: {username: req.body.username}});
-  res.status(200).json(user);
-});
-
-userRouter.get('/userBasic', basic, async(req, res) => {
-  const user = await users.findOne({ where: {username: req.body.username}});
-  res.status(200).json(user);
-});
 
 userRouter.post('/register', async (req, res, next) => {
   try {
@@ -39,64 +22,59 @@ userRouter.post('/register', async (req, res, next) => {
 
 // Users NFT Routes ('likes')
 // TODO: GET all likes
+
 // POST: Add to likes
 userRouter.post('/nft/like/:id', bearer, async (req, res, next) => {
   try {
     if(!req.params.id) { throw new Error('Please provide a valid ID');}
 
-    // Stretch: If you already like something just unlike it.
-    let userQuery = await users.findOne({ where: {username: req.body.username}})
+    // Remove & Append to existing array https://stackoverflow.com/a/44365347
+    let userQuery = await users.findOne({ where: {username: req.body.username}});
+    let userRecord;
     if(userQuery.likes.includes(req.params.id)) {
-      throw new Error('nft already liked!')
+      userRecord = await users.update(
+        {'likes': db.fn('array_remove', db.col('likes'), req.params.id)},
+        {'where': {'username': req.body.username}}
+      );
+    } else {
+      userRecord = await users.update(
+        {'likes': db.fn('array_append', db.col('likes'), req.params.id)},
+        {'where': {'username': req.body.username}}
+      );
     }
 
-    // Append to existing array https://stackoverflow.com/a/44365347
-    let userRecord = await users.update(
-      {'likes': db.fn('array_append', db.col('likes'), req.params.id)},
-      {'where': {'username': req.body.username}}
-    );
-    
-    const output = {
-      user: userRecord,
-      userToken: userRecord.token,
-    };
-
-    res.status(201).json(output);
+    res.status(201).json({updatedLikes: userRecord});
   } catch (err) {
     next(err.message);
   }
 });
-// TODO: DELETE nft like
 
 // Users Followed Address Routes ('follows')
 // TODO: Get all follows
-// POST: Add to follows
-userRouter.post('/address/follows/:address', bearer, async (req, res, next) => {
-  try {
-    if(!req.params.id) { throw new Error('Please provide a valid address');}
 
-    // Stretch: If you already like something just unlike it.
-    let userQuery = await users.findOne({ where: {username: req.body.username}})
+// POST: Add to follows
+userRouter.post('/address/follow/:address', bearer, async (req, res, next) => {
+  try {
+    if(!req.params.address && typeof req.params.address === 'string') { throw new Error('Please provide a valid address');}
+
+    let userQuery = await users.findOne({ where: {username: req.body.username}});
+    let userRecord;
     if(userQuery.follows.includes(req.params.address)) {
-      throw new Error('nft already liked!')
+      userRecord = await users.update(
+        {'follows': db.fn('array_remove', db.col('follows'), req.params.address)},
+        {'where': {'username': req.body.username}}
+      );
+    } else {
+      userRecord = await users.update(
+        {'follows': db.fn('array_append', db.col('follows'), req.params.address)},
+        {'where': {'username': req.body.username}}
+      );
     }
 
-    // https://stackoverflow.com/a/44365347
-    let userRecord = await users.update(
-      {'follows': db.fn('array_append', db.col('follows'), req.params.address)},
-      {'where': {'username': req.body.username}}
-    );
-    
-    const output = {
-      user: userRecord,
-      userToken: userRecord.token,
-    };
-
-    res.status(201).json(output);
+    res.status(201).json({updatedFollows: userRecord});
   } catch (err) {
     next(err.message);
   }
 });
-// TODO DELETE follows
 
 module.exports = userRouter;
