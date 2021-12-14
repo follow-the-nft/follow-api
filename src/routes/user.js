c6a216b7ecf85d5d82e08f2acb17fe844eb96ca8
Charlie <boy_916@hotmail.com>
2021-12-13 22:29:45 -0800

    added get routes for likes and users

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,22 +5,42 @@ const userRouter = express.Router();
 
 const { users, db } = require('../models/index');
 const bearer = require('../middleware/bearer');
-const basic = require('../middleware/basic');
 
 // GET User - User Reads
-userRouter.get('/user', async(req, res) => {
-  const userData = await users.findOne({ where: {username: req.body.username}});
-  res.status(200).json(userData);
-});
- 
-userRouter.get('/userBearer', bearer, async(req, res) => {
-  const user = await users.findOne({ where: {username: req.body.username}});
-  res.status(200).json(user);
+// TODO: Return back all created Users
+userRouter.get('/users', async(req, res) => {
+  try {
+    const userData = await users.findOne({ where: {username: req.body.username}});
+    res.status(200).json(userData);
+  } catch (e) {
+    console.error('user likes Error');
+  }
 });
 
-userRouter.get('/userBasic', basic, async(req, res) => {
-  const user = await users.findOne({ where: {username: req.body.username}});
-  res.status(200).json(user);
+// Returns an Array of Likes or a string on an empty array
+// Done: GET all likes
+userRouter.get('/user/likes', bearer, async(req, res) => {
+  try {
+    const user = await users.findOne({ where: {username: req.body.username}});
+    let results = user.dataValues.likes;
+    if(results !== []) res.status(200).send('You do not have anything liked in here!');
+    res.status(200).json(results);
+  } catch (e) {
+    console.error('user likes Error');
+  }
+});
+
+// Returns an Array of Follows or a string on an empty array
+// Done: Get all follows
+userRouter.get('/user/follows', bearer, async(req, res) => {
+  try {
+    const user = await users.findOne({ where: {username: req.body.username}});
+    let results = user.dataValues.follows;
+    if(results !== []) res.status(200).send('You do not followed anything liked in here!');
+    res.status(200).json(results);
+  } catch (e) {
+    console.error(e, 'user likes Error');
+  }
 });
 
 userRouter.post('/register', async (req, res, next) => {
@@ -36,18 +56,16 @@ userRouter.post('/register', async (req, res, next) => {
   }
 });
 
-
-// Users NFT Routes ('likes')
-// TODO: GET all likes
 // POST: Add to likes
 userRouter.post('/nft/like/:id', bearer, async (req, res, next) => {
   try {
     if(!req.params.id) { throw new Error('Please provide a valid ID');}
 
     // Stretch: If you already like something just unlike it.
-    let userQuery = await users.findOne({ where: {username: req.body.username}})
+    let userQuery = await users.findOne({ where: {username: req.body.username}});
+    console.log('userQuery:', userQuery);
     if(userQuery.likes.includes(req.params.id)) {
-      throw new Error('nft already liked!')
+      throw new Error('nft already liked!');
     }
 
     // Append to existing array https://stackoverflow.com/a/44365347
@@ -66,19 +84,17 @@ userRouter.post('/nft/like/:id', bearer, async (req, res, next) => {
     next(err.message);
   }
 });
-// TODO: DELETE nft like
 
 // Users Followed Address Routes ('follows')
-// TODO: Get all follows
 // POST: Add to follows
 userRouter.post('/address/follows/:address', bearer, async (req, res, next) => {
   try {
     if(!req.params.id) { throw new Error('Please provide a valid address');}
 
     // Stretch: If you already like something just unlike it.
-    let userQuery = await users.findOne({ where: {username: req.body.username}})
+    let userQuery = await users.findOne({ where: {username: req.body.username}});
     if(userQuery.follows.includes(req.params.address)) {
-      throw new Error('nft already liked!')
+      throw new Error('nft already liked!');
     }
 
     // https://stackoverflow.com/a/44365347
@@ -97,6 +113,5 @@ userRouter.post('/address/follows/:address', bearer, async (req, res, next) => {
     next(err.message);
   }
 });
-// TODO DELETE follows
 
 module.exports = userRouter;
