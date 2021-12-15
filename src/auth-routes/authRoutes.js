dbf2309a41f4949b97444f576986ba41a3ea3064
MuckT <mcgtom10@gmail.com>
2021-12-15 04:20:44 -0800

    test: move all db tests into one file

--- src/auth-routes/authRoutes.js
+++ src/auth-routes/authRoutes.js
@@ -8,22 +8,11 @@ const basicAuth = require('../middleware/basic.js');
 const bearerAuth = require('../middleware/bearer');
 const permissions = require('../middleware/acl.js');
 
-authRouter.get('/testBasic',  basicAuth, async () => {
-  console.log('passed the test');
-});
-
-authRouter.get('/testBearer',  bearerAuth, async () => {
-  console.log('passed the test');
-});
-
 authRouter.post('/register', async (req, res, next) => {
-  console.log('i made it');
   try {
     let userRecord = await users.create(req.body);
-    console.log('this is req dot body', req.body);
     const output = {
-      user: userRecord,
-      token: userRecord.token,
+      user: userRecord
     };
     res.status(201).json(output);
   } catch (e) {
@@ -40,7 +29,6 @@ authRouter.post('/signin', basicAuth, (req, res) => {
 });
 
 authRouter.get('/users', bearerAuth, permissions('read'), async (req, res) => {
-  console.log('i made it to users');
   const userRecords = await users.findAll({});
   const list = userRecords.map((user) => user.username);
   res.status(200).json(list);
--- src/routes/auth-routes.test.js
+++ src/routes/auth-routes.test.js
@@ -1,5 +1,5 @@
 const supertest = require('supertest');
-const { db, users, likes } = require('../models/index');
+const { db, users, follows, likes } = require('../models/index');
 const { server } = require('../server');
 const request = supertest(server);
 
@@ -7,18 +7,26 @@ beforeAll(async () => {
   await db.sync();
   await users.bulkCreate([
     {
-      id: 23,
+      id: 24,
       username: 'TEST',
       password: '$2a$10$fYHN0Tgp3SqUlOclOaL/TucaNVLyz6JKS29KTTYbbjcu4iQPyiQgC',
       updatedAt: new Date('2021-10-25'),
       createdAt: new Date('2021-10-25')
     },
-
+  ]);
+  await follows.bulkCreate([
+    {
+      id: 42,
+      user_id: 24,
+      contract_address: '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB',
+      updatedAt: new Date('2021-10-25'),
+      createdAt: new Date('2021-10-25')
+    },
   ]);
   await likes.bulkCreate([
     {
       id: 42,
-      user_id: 23,
+      user_id: 24,
       contract_address: '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB',
       token_id: 15,
       updatedAt: new Date('2021-10-25'),
@@ -31,15 +39,49 @@ afterAll(async () => {
   await db.drop();
 });
 
+describe('Given /newFollows', () => {
+  describe('When Authenticated', () => {
+    it('Then user can retrieve follows', async () => {
+      let user = await users.findOne({ where: { id: 24} });
+      const response = await request.get('/newFollows').send(user).set('Authorization', `Bearer ${user.token}`);
+      expect(response.body).toStrictEqual([
+        {
+          id: 42,
+          user_id: 24,
+          contract_address: '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB',
+          createdAt: '2021-10-25T00:00:00.000Z',
+          updatedAt: '2021-10-25T00:00:00.000Z'
+        }
+      ]);
+    });
+
+    it('Then user can delete a like', async () => {
+      let user = await users.findOne({ where: { id: 24} });
+      const response = await request.delete('/newFollows/0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB').send(user).set('Authorization', `Bearer ${user.token}`);
+      expect(response.status).toStrictEqual(204);
+      expect(response.body).toStrictEqual({});
+    });
+
+    it('Then user can create a like', async () => {
+      let user = await users.findOne({ where: { id: 24} });
+      const response = await request.post('/newFollows/0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb').send(user).set('Authorization', `Bearer ${user.token}`);
+      expect(response.status).toStrictEqual(200);
+      expect(response.body.id).toStrictEqual(43);
+      expect(response.body.user_id).toStrictEqual(24);
+      expect(response.body.contract_address).toStrictEqual('0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb');
+    });
+  });
+});
+
 describe('Given /newLikes', () => {
   describe('When Authenticated', () => {
     it('Then user can retrieve likes', async () => {
-      let user = await users.findOne({ where: { id: 23} });
+      let user = await users.findOne({ where: { id: 24} });
       const response = await request.get('/newLikes').send(user).set('Authorization', `Bearer ${user.token}`);
       expect(response.body).toStrictEqual([
         {
           id: 42,
-          user_id: 23,
+          user_id: 24,
           contract_address: '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB',
           token_id: '15',
           createdAt: '2021-10-25T00:00:00.000Z',
@@ -49,14 +91,14 @@ describe('Given /newLikes', () => {
     });
 
     it('Then user can delete a like', async () => {
-      let user = await users.findOne({ where: { id: 23} });
+      let user = await users.findOne({ where: { id: 24} });
       const response = await request.delete('/newLikes/0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB/15').send(user).set('Authorization', `Bearer ${user.token}`);
       expect(response.status).toStrictEqual(204);
       expect(response.body).toStrictEqual({});
     });
 
     // This test fails because the db is deleted by this point
-    it.skip('Then user can create a like', async () => {
+    it('Then user can create a like', async () => {
       let user = await users.findOne({ where: { id: 24} });
       const response = await request.post('/newLikes/0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB/42').send(user).set('Authorization', `Bearer ${user.token}`);
       expect(response.status).toStrictEqual(200);
@@ -66,4 +108,4 @@ describe('Given /newLikes', () => {
       expect(response.body.token_id).toStrictEqual('42');
     });
   });
-});
+})
